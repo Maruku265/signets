9256ee9368bace08f4f42f467a20d87b733afa27
Camille Semaan <cdem@xvitae.com>
2021-03-11 19:58:18 -0500

    Petit oubli.

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,7 @@ import 'firebase/firestore';
 
 // Config
 const firebaseConfig = {
-  apiKey: "AIzaSyCSRTbHzkt3npbWoSBfI0cfC2jnCkYdq7k",
-  authDomain: "cdem-react.firebaseapp.com",
-  projectId: "cdem-react",
-  storageBucket: "cdem-react.appspot.com",
-  messagingSenderId: "344031190712",
-  appId: "1:344031190712:web:7b7744b6272f5225305e4d"
+  
 };
 
 // Initialiser Firebase
