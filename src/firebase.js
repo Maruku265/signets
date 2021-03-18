f7fc6e0345cfb27cf9f7576c2fde51a6cdd9acbe
Maruku <marcp265@gmail.com>
2021-03-18 14:42:57 -0400

    added firebaseconfi

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,13 @@ import 'firebase/firestore';
 
 // Config
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyBVNZayndvVPJI-c0dOF-KA6BuGYjd2s98",
+  authDomain: "pvt-h21-pilon-marc.firebaseapp.com",
+  databaseURL: "https://pvt-h21-pilon-marc-default-rtdb.firebaseio.com",
+  projectId: "pvt-h21-pilon-marc",
+  storageBucket: "pvt-h21-pilon-marc.appspot.com",
+  messagingSenderId: "218748352260",
+  appId: "1:218748352260:web:68c0919b23be78067f0251"
 };
 
 // Initialiser Firebase
